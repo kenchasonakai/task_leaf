c1c123a4c7fd023f301835dbcb8914756dd5d964
kenchaso <kenchasonakai@gmail.com>
2021-12-16 19:05:25 +0900

    add: User用のコントローラーの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,38 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-  end
-
-  def edit
-  end
-
-  def update
+    @user = User.new(user_params)
+    if @user.save
+      #auto_login(user)
+      redirect_to root_path, notice: 'TaskLeafへようこそ'
+    else
+      render :new
+    end
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: 'ユーザー情報を更新しました'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
