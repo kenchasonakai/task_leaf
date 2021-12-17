6fd73e399ba41ea933194063ae8987d10b9beb01
kenchaso <kenchasonakai@gmail.com>
2021-12-17 15:55:51 +0900

    add: taskとuserにアソシエーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :name, presence: true
   validates :email, uniqueness: true, presence: true
+
+  has_many :tasks
 end
