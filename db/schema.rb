96cdf2b831e4b80f249f766009f71b1553e573eb
kenchaso <kenchasonakai@gmail.com>
2021-12-17 15:54:49 +0900

    add: TaskとUserの紐付け

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_16_055148) do
+ActiveRecord::Schema.define(version: 2021_12_17_065200) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", null: false
     t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
