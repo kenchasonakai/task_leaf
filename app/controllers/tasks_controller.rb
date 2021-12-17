6fb1478ad372ec6832149d79fff68bb2f9aa6d78
kenchaso <kenchasonakai@gmail.com>
2021-12-17 16:05:44 +0900

    add: taskの更新 削除を自分が作ったタスクのみ出来るように制御を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,16 @@
 class TasksController < ApplicationController
+  skip_before_action :require_login, only: %w[index show]
+
   def index
     @tasks = Task.all
   end
 
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     if @task.save
       redirect_to tasks_url, success: "タスク[#{@task.name}]を登録しました"
     else
@@ -21,11 +23,11 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
     if @task.update(task_params)
       redirect_to tasks_url, success: "タスク[#{@task.name}]を更新しました"
     else
@@ -34,7 +36,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    task = Task.find(params[:id])
+    task = current_user.tasks.find(params[:id])
     task.destroy!
     redirect_to tasks_url, success: "タスク[#{task.name}]を削除しました"
   end
