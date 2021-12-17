2b6e1bf8fd8b9e88bb4499d7f6512a739d565b4e
kenchaso <kenchasonakai@gmail.com>
2021-12-17 15:11:24 +0900

    add: user_sessions_controllerの処理追加

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,10 +1,19 @@
 class UserSessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
+    @user = login(params[:email], params[:password])
+
+    if @user
+      redirect_back_or_to(root_path, notice: 'ログインしました')
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+      render action: 'new'
+    end
   end
 
   def destroy
+    logout
+    redirect_to(root_path, notice: 'ログアウトしました')
   end
 end
